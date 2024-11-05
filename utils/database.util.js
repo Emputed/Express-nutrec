96c44bf577b0629d1b3ce1937960886f323e062b
emputed <amejoradan1600@alumno.ipn.mx>
2024-11-04 18:09:29 -0600

    Se realizo la conexion a la base de datos en Azure

--- utils/database.util.js
+++ utils/database.util.js
@@ -1,4 +1,5 @@
-import { config } from 'dotenv';config();
+import { config } from "dotenv";
+config();
 import Sequelize from "sequelize";
 //import fs from 'fs';
 //import path from 'path';
@@ -18,25 +19,27 @@ const HOST = process.env.HOST;
     ca: fs.readFileSync(filePath),
 };*/
 
-const sequelize = new Sequelize(
-    DATABASE,
-    USER,
-    PASSWORD,
-    {
-        host: HOST,
-        dialect: "mysql",
-        port: 3306
-    }
-);
+const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
+  host: HOST,
+  dialect: "mysql",
+  port: 3306,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Esto permite conexiones con certificados no verificados, como los de Azure.
+    },
+  },
+  logging:false
+});
 
-async function testConnection(){
-    try{    
-        await sequelize.authenticate();
-        console.log("All set");
-    }catch(err){
-        console.log(err);
-    }
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("All set");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 testConnection();
-export default sequelize
+export default sequelize;
